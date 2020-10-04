b20ed3d67affcfdac089e1ccafca2e369bf90859
Saturnin Dao <saturnindao@gmail.com>
2020-10-04 16:24:57 +0000

    products json emit on node/express server

---
+++ renvoiDeDonnees.js
@@ -0,0 +1,19 @@
+var express = require('express'); 
+var fs      = require('fs'); 
+var app     = express(); 
+ 
+app.get('/products', (req, res) => { 
+   res.setHeader('Content-Type', 'application/json; charset=utf-8'); 
+   res.setHeader('Access-Control-Allow-Origin', '*'); 
+ 
+   var readable = fs.createReadStream('Products.json'); 
+   readable.on('open', () => { 
+            readable.pipe(res); 
+            console.log('Liste des produits renvoyée !'); 
+        });  
+   readable.on('error', () => { 
+       res.end('[]'); 
+    }); 
+}); 
+ 
+app.listen(8889);
